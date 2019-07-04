6a330ec353b0a673cc28b5cd824fb69c9c0f8093
Craig Quiter <cquiter@gmail.com>
2019-07-03 17:57:41 -0700

    pip8 columns

--- deepdrive_api/constants.py
+++ deepdrive_api/constants.py
@@ -4,8 +4,12 @@ DEFAULT_CAM = dict(name='My cam!',
                    field_of_view=60,
                    capture_width=227,
                    capture_height=227,
-                   relative_position=[150, 1.0, 200],  # forward, right, up in cm
-                   relative_rotation=[0.0, 0.0, 0.0])  # roll, pitch, yaw in degrees
+
+                   # forward, right, up in cm
+                   relative_position=[150, 1.0, 200],
+
+                   # roll, pitch, yaw in degrees
+                   relative_rotation=[0.0, 0.0, 0.0])
 
 API_PORT = 5557
 SIM_HOST = os.environ.get('DEEPDRIVE_SIM_HOST', 'localhost')

0592ecad363eae7b59a0c47e5ff7207b9ff018a8
Craig Quiter <cquiter@gmail.com>
2019-07-03 22:24:36 -0700

    Avoid duplicate log messages

--- deepdrive_api/logs.py
+++ deepdrive_api/logs.py
@@ -22,6 +22,7 @@ def get_log(namespace):
         ch = logging.StreamHandler(sys.stdout)
         ch.setFormatter(log_format)
         ret.addHandler(ch)
+        ret.propagate = False
     return ret
 
 

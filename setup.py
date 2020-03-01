21977840cdedf6a4a2804ffaf0074b64823c6c3d
Craig Quiter <cquiter@gmail.com>
2020-03-01 11:49:39 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(abspath(join(dirname(__file__), 'README.md')), 'rb') as readme_file:
 
 setup(
     name='deepdrive-api',
-    version='3.1.20191111204349',
+    version='3.1.20200301111111',
     description='Deepdrive API used to run agents over the network',
     long_description=__readme__,
     long_description_content_type='text/markdown',
